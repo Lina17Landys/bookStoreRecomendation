7f489548a25bfe4908f55dc54868788f16a155e3
JuanVB1 <131167099+juanvelasco1@users.noreply.github.com>
2025-05-23 11:22:20 -0500

    FIxFirebase

--- src/services/firebaseConfig.js
+++ src/services/firebaseConfig.js
@@ -1,17 +1,21 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyDoe3wYxX0xL_VJwwOUucX7Jp0PGAEgxrU",
-  authDomain: "webb-f728b.firebaseapp.com",
-  projectId: "webb-f728b",
-  storageBucket: "webb-f728b.firebasestorage.app",
-  messagingSenderId: "310694459401",
-  appId: "1:310694459401:web:03a925d5d91d0b266a20d8",
-  measurementId: "G-LCRWL0JYC3"
+  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const db = getFirestore(app);
+const auth = getAuth(app);
 
-export { app, analytics };
+export { app, analytics, db, auth };
