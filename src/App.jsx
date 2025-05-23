2e10c88d94d52f618365786338e292dad00decfe
JuanVB1 <131167099+juanvelasco1@users.noreply.github.com>
2025-05-23 11:26:44 -0500

    fix1

--- src/App.jsx
+++ src/App.jsx
@@ -8,17 +8,25 @@ import RegisterPage from './pages/RegisterPage/RegisterPage';
 import SelectionPage1 from './pages/SelectionPage1/SelectionPage1';
 import SelectionPage2 from './pages/SelectionPage2/SelectionPage2';
 import SelectionPage3 from './pages/SelectionPage3/SelectionPage3';
+import LandingPage from './pages/LandingPage/LandingPage';
+import LibrosPage from './pages/LibrosPage/LibrosPage';
+import UsuarioPage from './pages/UsuarioPage/UsuarioPage';
+import EditarPerfilPage from './pages/EditarPerfilPage/EditarPerfilPage';
 
 function App() {
   return (
     <Router>
       <div id="app-wrapper">
         <Routes>
+          <Route path="/landing" element={<LandingPage />} />
           <Route path="/" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="/selection1" element={<SelectionPage1 />} />
           <Route path="/selection2" element={<SelectionPage2 />} />
           <Route path="/selection3" element={<SelectionPage3 />} />
+          <Route path="/libros" element={<LibrosPage />} />
+          <Route path="/usuario" element={<UsuarioPage />} />
+          <Route path="/editar-perfil" element={<EditarPerfilPage />} />
         </Routes>
       </div>
     </Router>
