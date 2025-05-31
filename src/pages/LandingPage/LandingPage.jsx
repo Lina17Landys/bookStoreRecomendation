d45b0191c1416582a55bbbadcb566065bfb4f36f
Lina Marcela Landys Samboni <lina_landys@Linaaa.local>
2025-05-31 09:46:15 -0500

    add libros mas recomendados y correcion de css general

--- src/pages/LandingPage/LandingPage.jsx
+++ src/pages/LandingPage/LandingPage.jsx
@@ -1,7 +1,7 @@
 import Landing from '../../components/Landing/Landing';
 import Barra from '../../components/barra/barra';
 import Features from '../../components/features/features';
-
+import BooksFamosos from '../../components/booksFamosos/booksfamosos';
 
 const LandingPage = () => {
   return (
@@ -9,7 +9,7 @@ const LandingPage = () => {
       <Barra />
       <Landing />
       <Features />
-
+      <BooksFamosos />
     </div>
   );
 };
